aea010a577f454c3e58988ec22443f2a81d2d082
dbdudann <dbdudann@gmail.com>
2024-01-08 19:43:44 +0900

    Initial commit

---
+++ src/main/java/com/simpo/simplepost/SimplePostApplication.java
@@ -0,0 +1,13 @@
+package com.simpo.simplepost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplePostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplePostApplication.class, args);
+	}
+
+}
