031b96be36b4a1cc80f237b32274acfd18facaa2
dbdudann <dbdudann@gmail.com>
2024-01-10 09:44:30 +0900

    Feat: Post Entity 생성

---
+++ src/main/java/com/simpo/simplepost/post/entity/Post.java
@@ -0,0 +1,40 @@
+package com.simpo.simplepost.post.entity;
+
+import com.simpo.simplepost.common.BaseEntity;
+import com.simpo.simplepost.board.entity.Board;
+import com.simpo.simplepost.comment.entity.Comment;
+import com.simpo.simplepost.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Entity
+public class Post extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_id")
+    private Long id;
+
+    private String title;
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "board_id")
+    private Board board;
+
+    @OneToMany(mappedBy = "post")
+    private List<Comment> comments;
+
+
+}
